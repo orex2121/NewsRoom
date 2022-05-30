593705836912f1a0be79ef0923c5877b45e60c8e
orex2121 <orex2121@gmail.com>
2022-05-30 19:40:48 +0300

    lesson 3

---
+++ app/src/main/java/ru/jandroid/newsroom/models/Article.kt
@@ -0,0 +1,12 @@
+package ru.jandroid.newsroom.models
+
+data class Article(
+    val author: Any,
+    val content: Any,
+    val description: Any,
+    val publishedAt: String,
+    val source: Source,
+    val title: String,
+    val url: String,
+    val urlToImage: Any
+)
---
+++ app/src/main/java/ru/jandroid/newsroom/utils/Constants.kt
@@ -0,0 +1,8 @@
+package ru.jandroid.newsroom.utils
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "ecadb70d989043508cfbea7363e10a77"
+    }
+}
